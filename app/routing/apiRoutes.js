05e582b9d14c734062893065698f8715532a1b9c
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-27 19:46:16 -0400

    Adding basic API Routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+//Returns all potential friends
+
+app.get("/api/friends", function(req, res){
+    return res.json(friends);
+});
+
+app.post("/api/friends", function(req, res){
+    var newFriend = req.body;
+
+    console.log(newFriend);
+
+    friends.push(newFriend);
+
+    res.json(newFriend)
+})
