b74c297b6da798e36f2e077ead075fd901389ad5
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-27 20:23:36 -0400

    Making htmlRoutes default to homepage

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,15 @@ var express = require('express');
 var htmlRoutes = express.Router();
 var path = require('path');
 
-htmlRoutes.get("/", function (req, res) {
-
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 htmlRoutes.get("/survey", function (req, res) {
 
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+htmlRoutes.get("*", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = htmlRoutes;
