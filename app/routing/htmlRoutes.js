32d2f914b1f398d6784ee55101d5c29311c36989
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-27 20:19:53 -0400

    Adding basic outlines for html pages.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@ var htmlRoutes = express.Router();
 var path = require('path');
 
 htmlRoutes.get("/", function (req, res) {
-    console.log('sending index');
+
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 htmlRoutes.get("/survey", function (req, res) {
-    console.log('sending route');
+
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
