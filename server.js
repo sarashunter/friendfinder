f9c2186b6ae87f31b100e976951449eca3189a7c
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-29 19:27:59 -0400

    Adding rudimentary form that works with POST

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //External Dependencies
 var express = require("express");
-// var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 //Internal Dependencies
@@ -16,9 +16,9 @@ var PORT = process.env.PORT || 3000;
 app.use(require('./app/routing/apiRoutes'));
 app.use(require('./app/routing/htmlRoutes'));
 
-// //Use bodyParser for data parsing
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+//Use bodyParser for data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Start the server listening.
 app.listen(PORT, function() {
