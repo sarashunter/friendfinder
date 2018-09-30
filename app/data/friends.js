62b73d476c8c7e7c88fa6e367318d064863535d1
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-29 20:08:27 -0400

    Adding two questions which work with POST to friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         'name': 'John Smith',
-        'photo': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        'imgsrc': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
         'scores': [
             5,
             1,
